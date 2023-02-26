d6ba03dc553fd771d329d99236cc74c32b3fe045
Richo Healey <richo@psych0tik.net>
2023-02-25 20:49:22 -0800

    So fucking broken

--- silvia-controller/src/main.rs
+++ silvia-controller/src/main.rs
@@ -1,13 +1,30 @@
 #![no_std]
 #![no_main]
+#![feature(abi_avr_interrupt)]
 
 use panic_halt as _;
 use arduino_hal::prelude::*;
-use arduino_hal::hal::port::{Pin, PB2, PB3, PC4};
-use arduino_hal::hal::port::mode::{Input, Output, PullUp};
+use arduino_hal::hal::port::{Pin, PB1, PB2, PB3, PB5, PC4, PC5};
+use arduino_hal::hal::port::mode::{Input, Output, PullUp, Floating};
 
-type PUMP = Pin<Output, PB2>;
-type VALVE = Pin<Output, PB3>;
+type PUMP = Pin<Output, PC4>;
+type VALVE = Pin<Output, PC5>;
+
+use core::sync::atomic::{AtomicBool, Ordering};
+
+static BREW: AtomicBool = AtomicBool::new(false);
+#[avr_device::interrupt(atmega328p)]
+#[allow(non_snake_case)]
+fn PCINT2() { // D10
+        BREW.store(true, Ordering::SeqCst);
+}
+
+static BACKFLUSH: AtomicBool = AtomicBool::new(false);
+#[avr_device::interrupt(atmega328p)]
+#[allow(non_snake_case)]
+fn PCINT1() { // D9
+        BACKFLUSH.store(true, Ordering::SeqCst);
+}
 
 #[arduino_hal::entry]
 fn main() -> ! {
@@ -29,15 +46,31 @@ fn main() -> ! {
 
     let mut led = pins.d13.into_output();
 
-    let brew =  pins.a4.into_pull_up_input();
-    let backflush =  pins.a5.into_pull_up_input();
+    let brew =  pins.d9.into_pull_up_input();
+    let backflush =  pins.d10.into_floating_input();
 
-    let mut pump = pins.d10.into_output();
-    let mut valve = pins.d11.into_output();
+    let mut pump = pins.a4.into_output();
+    let mut valve = pins.a5.into_output();
     init_pins(&mut pump, &mut valve);
+    dp.EXINT.eicra.modify(|_, w| w.isc0().bits(0x01));
+    dp.EXINT.eimsk.modify(|_, w| w.int0().set_bit());
+    dp.EXINT.pcicr.write(|w| unsafe { w.bits(0xff) });
+    unsafe { avr_device::interrupt::enable() };
+    // core::sync::atomic::compiler_fence(core::sync::atomic::Ordering::SeqCst);
 
     loop {
         led.toggle();
+        let b = BREW.load(Ordering::SeqCst);
+        let ba = BACKFLUSH.load(Ordering::SeqCst);
+        ufmt::uwriteln!(&mut serial, "{:?} {:?}", b, ba).void_unwrap();
+        spin_wait();
+    }
+
+    loop {
+        led.toggle();
+        let b = BREW.load(Ordering::SeqCst);
+        let ba = BACKFLUSH.load(Ordering::SeqCst);
+        ufmt::uwriteln!(&mut serial, "{:?} {:?}", b, ba).void_unwrap();
         // TODO(richo) Migrate to doing an interrupt thing here instead of shitty histerisis
         if brew.is_low() {
             ufmt::uwriteln!(&mut serial, "brew switch").void_unwrap();
@@ -73,7 +106,7 @@ fn main() -> ! {
     }
 }
 
-fn init_pins(pump: &mut Pin<Output, PB2>, valve: &mut Pin<Output, PB3>) {
+fn init_pins(pump: &mut Pin<Output, PC4>, valve: &mut Pin<Output, PC5>) {
     pump.set_low();
     valve.set_low();
 }
@@ -88,6 +121,7 @@ const INFUSE_MILLIS: u16 = 2000;
 const INFUSE_WAIT_MILLIS: u16 = 2500;
 
 fn run_infuse(pump: &mut PUMP, valve: &mut VALVE) {
+    return;
     // Infuse the puck by closing the solenoid and running the pump, but do not open the valve when
     // finished.
     valve.set_high();
@@ -104,7 +138,8 @@ enum BrewConclusion {
 
 /// Confirm the solenoid is closed, then run the brew pump for some configurable number of millies,
 /// then turn off the pump and the solenoid
-fn run_brew(pump: &mut PUMP, valve: &mut VALVE, switch: &Pin<Input<PullUp>, PC4>) -> BrewConclusion {
+fn run_brew(pump: &mut PUMP, valve: &mut VALVE, switch: &Pin<Input<PullUp>, PB1>) -> BrewConclusion {
+    return BrewConclusion::Finished;
     // Infuse leaves the valve closed, but we'll double check
     valve.set_high();
     pump.set_high();
@@ -128,6 +163,7 @@ const BACKFLUSH_ON_MILLIS: u16 = 5000;
 const BACKFLUSH_PAUSE_MILLIS: u16 = 9000;
 
 fn run_backflush(pump: &mut PUMP, valve: &mut VALVE) {
+    return;
     for _ in 0..BACKFLUSH_REPEATS {
         valve.set_high();
         pump.set_high();
