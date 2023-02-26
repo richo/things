171653113a59deaeaadbafdfd8c7be6029085ed1
Richo Healey <richo@psych0tik.net>
2023-02-25 21:04:01 -0800

    Bigger refactor

--- silvia-controller/src/main.rs
+++ silvia-controller/src/main.rs
@@ -3,7 +3,7 @@
 
 use panic_halt as _;
 use arduino_hal::prelude::*;
-use arduino_hal::hal::port::{Pin, PB2, PB3, PC4};
+use arduino_hal::hal::port::{Pin, PB2, PB3, PC4, PC5};
 use arduino_hal::hal::port::mode::{Input, Output, PullUp};
 
 type PUMP = Pin<Output, PB2>;
@@ -43,15 +43,17 @@ fn main() -> ! {
             ufmt::uwriteln!(&mut serial, "brew switch").void_unwrap();
 
             ufmt::uwriteln!(&mut serial, "starting infuse").void_unwrap();
-            run_infuse(&mut pump, &mut valve);
+            if let Conclusion::Stopped = run_infuse(&mut pump, &mut valve, &brew) {
+                continue
+            }
             ufmt::uwriteln!(&mut serial, "infusion finished").void_unwrap();
             ufmt::uwriteln!(&mut serial, "starting brew").void_unwrap();
             let res = run_brew(&mut pump, &mut valve, &brew);
             match res {
-                BrewConclusion::Finished => {
+                Conclusion::Finished => {
                     ufmt::uwriteln!(&mut serial, "brew finished").void_unwrap();
                 },
-                BrewConclusion::Stopped => {
+                Conclusion::Stopped => {
                     ufmt::uwriteln!(&mut serial, "brew interupted").void_unwrap();
                 },
             }
@@ -60,16 +62,16 @@ fn main() -> ! {
             }
         } else if backflush.is_low() {
             ufmt::uwriteln!(&mut serial, "backflush switch").void_unwrap();
-            run_backflush(&mut pump, &mut valve);
+            run_backflush(&mut pump, &mut valve, &backflush);
             while backflush.is_low() {
                 spin_wait();
             }
             ufmt::uwriteln!(&mut serial, "backflush finished").void_unwrap();
         }
 
-        spin_wait();
         // Set them low on every iteration just to be safe.
         init_pins(&mut pump, &mut valve);
+        spin_wait();
     }
 }
 
@@ -87,39 +89,52 @@ fn spin_wait() {
 const INFUSE_MILLIS: u16 = 2000;
 const INFUSE_WAIT_MILLIS: u16 = 2500;
 
-fn run_infuse(pump: &mut PUMP, valve: &mut VALVE) {
+fn run_infuse(pump: &mut PUMP, valve: &mut VALVE, switch: &Pin<Input<PullUp>, PC4>) -> Conclusion {
     // Infuse the puck by closing the solenoid and running the pump, but do not open the valve when
     // finished.
     valve.set_high();
     pump.set_high();
-    arduino_hal::delay_ms(INFUSE_MILLIS);
+    if let Conclusion::Stopped = until_unless(INFUSE_MILLIS, || switch.is_low()) {
+        pump.set_low();
+        valve.set_low();
+        return Conclusion::Stopped;
+    }
     pump.set_low();
-    arduino_hal::delay_ms(INFUSE_WAIT_MILLIS);
+    until_unless(INFUSE_WAIT_MILLIS, || switch.is_low())
 }
 
-enum BrewConclusion {
+enum Conclusion {
     Finished,
     Stopped,
 }
 
 /// Confirm the solenoid is closed, then run the brew pump for some configurable number of millies,
 /// then turn off the pump and the solenoid
-fn run_brew(pump: &mut PUMP, valve: &mut VALVE, switch: &Pin<Input<PullUp>, PC4>) -> BrewConclusion {
+fn run_brew(pump: &mut PUMP, valve: &mut VALVE, switch: &Pin<Input<PullUp>, PC4>) -> Conclusion {
     // Infuse leaves the valve closed, but we'll double check
     valve.set_high();
     pump.set_high();
 
     // We'll run the pump for 35s or until someone stops us
     // TODO(richo) Again holy shit this should be an interrupt thing
-    // Whatever this doesn't actually need to be scientific
-    const RESOLUTION: u16 = 20;
-    for _ in 0..(35000 / RESOLUTION) {
-        if switch.is_low() {
-            return BrewConclusion::Stopped;
+    until_unless(3500, || switch.is_low())
+}
+
+
+const RESOLUTION: u16 = 20;
+fn until_unless<F>(millis: u16, unless: F) -> Conclusion
+where F: Fn() -> bool {
+    for i in 0..(millis / RESOLUTION) {
+        if unless() {
+            // Wait until the condition clears
+            while unless() {
+                arduino_hal::delay_ms(RESOLUTION);
+            }
+            return Conclusion::Stopped;
         }
         arduino_hal::delay_ms(RESOLUTION);
     }
-    return BrewConclusion::Finished;
+    Conclusion::Finished
 }
 
 /// Backflush the machine.
@@ -127,13 +142,15 @@ const BACKFLUSH_REPEATS: u16 = 5;
 const BACKFLUSH_ON_MILLIS: u16 = 5000;
 const BACKFLUSH_PAUSE_MILLIS: u16 = 9000;
 
-fn run_backflush(pump: &mut PUMP, valve: &mut VALVE) {
+fn run_backflush(pump: &mut PUMP, valve: &mut VALVE, switch: &Pin<Input<PullUp>, PC5>) {
     for _ in 0..BACKFLUSH_REPEATS {
         valve.set_high();
         pump.set_high();
-        arduino_hal::delay_ms(BACKFLUSH_ON_MILLIS);
+        let res = until_unless(BACKFLUSH_ON_MILLIS, || switch.is_low());
         pump.set_low();
         valve.set_low();
-        arduino_hal::delay_ms(BACKFLUSH_PAUSE_MILLIS);
+        if let Conclusion::Finished = res {
+            until_unless(BACKFLUSH_PAUSE_MILLIS, || switch.is_low());
+        }
     }
 }
