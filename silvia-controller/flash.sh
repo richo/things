85fa2c7236527a518d8530522e5234d05d9ae27b
Richo Healey <richo@psych0tik.net>
2023-03-01 10:58:50 -0800

    Helper

---
+++ silvia-controller/flash.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+if [ -z "$1" ]; then
+	echo "usage: $0 <bin>"
+	exit 1
+fi
+
+cargo build --bin "$1" && avrdude -patmega328p -carduino -P/dev/tty.usbserial-210 -b115200 -D -Uflash:w:target/avr-atmega328p/debug/"$1".elf:e && ravedude nano -P /dev/tty.usbserial-210 -c -b 57600
